04e789921f8c645f7d6056c8eb83016a89f7b05e
ryutaro asano <ryu@ryu-mac.local>
2020-05-04 21:52:37 +0900

    third commit

--- main.py
+++ main.py
@@ -83,14 +83,13 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 
-# Instance
+# Flask Web App Instance
 app = Flask(__name__)
 
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
@@ -98,6 +97,8 @@ if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
+
+# PREPARE LINE messaging API Instance
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
