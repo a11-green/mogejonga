ef835d79a128de27118c99092b433f4421ef1bb4
ryutaro asano <ryu@ryu-mac.local>
2020-05-04 16:51:45 +0900

    third commit

--- main.py
+++ main.py
@@ -63,8 +63,7 @@ from linebot.models import (
     QuickReplyButton
 )
 
-# AWS
-import boto3
+
 
 # DF,Graph,etc
 import matplotlib.pyplot as plt
@@ -75,20 +74,11 @@ import re # 正規表現
 
 # Instance
 app = Flask(__name__)
-fname = "test.txt" # データベース
 
-n1 = 0
-n2 = 0
-n3 = 0
-n4 = 0
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-aws_s3_bucket        = os.environ['AWS_BUCKET']
-s3 = boto3.resource("s3")
-bucket = s3.Bucket(aws_s3_bucket)
-s3_client = boto3.client('s3')
 
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
@@ -125,194 +115,90 @@ def message_text(event):
     テキストメッセージが送られてきたときの処理
     '''
     try:
-        message = event.message.text.split("\n") 
-        n = [0,0,0,0]
-        for j,line in enumerate(message):
-            if line.count("help") != 0:
-                # ヘルプ
-                message = "[ Apps help ]\n"
+        message = event.message.text
+        if message.count("plot") != 0:
                 
-            elif line.count("-") == 3 : 
-                # 着順を入力したときの動作
-                global data
-                data = line.split("-")
-                for i in range(4):
-                    n[i] += int(data[i])
-                message = "Are you sure?\n"
-                message += " 1着 : {}回\n 2着 : {}回\n 3着 : {}回\n 4着 : {}回\n".format(data[0],data[1],data[2],data[3])
-
-            elif line.count("円") == 1:
-                # "円"を含む行は収支として計算する
-                # さらに "+" or "勝"　を含めばプラス収支，
-                # "-" or "負" を含めばマイナス収支とする
-                global money
-                money = re.sub("\\D", "", line)
-                bucket.download_file(fname, fname)
-                if (line.count("+") == 1) or (line.count("勝")) == 1:
-                    money = abs(int(money))
-                if (line.count("-") == 1) or (line.count("負")) == 1:
-                    money = abs(int(money))*(-1.0)
-                message += "{} 円".format(int(money))
-                
-                
-            elif line.count("show") != 0 :
-                # データをみるときの動作
-                # "show" とうつと起動する
-                message = "data URL\n"
-                message += s3_client.generate_presigned_url(
-                    ClientMethod = 'get_object',
-                    Params       = {'Bucket': aws_s3_bucket, 'Key': fname},
-                    ExpiresIn    = 10,
-                    HttpMethod   = 'GET' )
-
-            elif line.count("del") != 0:
-                message = "deleted all !"
-                with open(fname,'w') as f:
-                    f.write("date,1st,2nd,3rd,4th,money\n")
-                bucket.upload_file(fname, fname)
-
-            elif line.count("stat") != 0:
-                # 成績の統計処理等を行う
-                # 選択肢をQuickReplyボタンで表示する
-                # Postback変数によって処理を変える
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(
-                        text="どれにする?",
-                        quick_reply=QuickReply(
-                            items=[
-                                QuickReplyButton(
-                                    action=PostbackAction(
-                                        label="着順分布",       # ボタンに表示する文字
-                                        text="着順分布をみせて",  # テキストとして送信する文字
-                                        data="request_pie"     # Postback
-                                    )
-                                ),
-                                QuickReplyButton(
-                                    action=PostbackAction(
-                                        label="合計",
-                                        text="合計をみせて",
-                                        data="request_sum"
-                                    )
-                                ),
-                                QuickReplyButton(
-                                    action=PostbackAction(
-                                        label="データ",
-                                        text="データをみせて",
-                                        data="request_data"
-                                    )
-                                ),
-                                QuickReplyButton(
-                                    action=PostbackAction(
-                                        label="収支履歴",
-                                        text="収支の履歴をみせて",
-                                        data="request_history"
-                                    )
-                                )
-                            ]
-                        )
-                    )
+            message = "plotting...\n"
+            line_bot_api.reply_message(
+                event.reply_token,
+                ImageSendMessage(
+                    original_content_url = "https://qiita.com/colorrabbit/items/18db3c97734f32ebdfde",
+                    preview_image_url    = "https://qiita.com/colorrabbit/items/18db3c97734f32ebdfde"
                 )
-            
-            
-
-
-            else :
-                text += "備考 : {}\n".format(line)
-            
-        confirm_template_message = TemplateSendMessage(
-            alt_text='Confirm template',
-            template=ConfirmTemplate(
-                text=message,
-                actions=[
-                    PostbackAction(
-                        label='yes',
-                        display_text='yes :)',
-                        data='action:yes'
-                    ),
-                    MessageAction(
-                        label='no',
-                        text='no XD'
-                    )
-                ]
             )
-        )
-        line_bot_api.reply_message(event.reply_token, messages=confirm_template_message)
-        
-    except Exception as e:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=e))
-        pass
 
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    '''
-    PostBackアクションがあったときの動作
-    '''
-    postbackdata = event.postback.data
-    if postbackdata == "action:yes":
-        # 着順を入力
-        bucket.download_file(fname, fname)
-        date = datetime.date.today()
-        with open(fname,'a') as f:
-            f.write("{},{},{},{},{}\n".format(date,data[0],data[1],data[2],data[3]))
-        # bucket.upload_file(fname, fname)
-        # 収支を入力
-        with open(fname) as f:
-            lines = f.readlines()
-            tmp = lines[-1].rstrip('\n') + ",{}\n".format(money)
-            lines[-1] = tmp
-        with open(fname,'w') as f:
-            for line in lines:
-                f.write(line)
-        bucket.upload_file(fname, fname)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="done!")
-        )
+            
 
-    elif postbackdata == "request_pie":
-        # 円グラフを表示
-        bucket.download_file(fname, fname)
-        df = pd.read_csv(fname,header=0)
-        plt.clf()
-        x = np.array([df["1st"].sum(),df["2nd"].sum(),df["3rd"].sum(),df["4th"].sum()])
-        plt.pie(x)
-        plt.legend()
-        plt.savefig("pie.png")
-        bucket.upload_file("pie.png", "pie.png")
-        s3_image_url = s3_client.generate_presigned_url(
-            ClientMethod = 'get_object',
-            Params       = {'Bucket': aws_s3_bucket, 'Key': "pie.png"},
-            ExpiresIn    = 10,
-            HttpMethod   = 'GET'
-        )
-        line_bot_api.reply_message(
-            event.reply_token,
-            ImageSendMessage(
-                original_content_url = s3_image_url,
-                preview_image_url    = s3_image_url,
-            )
-        )
-    elif postbackdata == "request_sum":
-        # 合計を表示
-        bucket.download_file(fname, fname)
-        df = pd.read_csv(fname,header=0)
-        x = np.array([df["1st"].sum(),df["2nd"].sum(),df["3rd"].sum(),df["4th"].sum()])
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text = "{}-{}-{}-{}".format(x[0],x[1],x[2],x[3]))
-        )
-    elif postbackdata == "request_data":
-        # データURLを表示
-        message = s3_client.generate_presigned_url(
-            ClientMethod = 'get_object',
-            Params       = {'Bucket': aws_s3_bucket, 'Key': fname},
-            ExpiresIn    = 10,
-            HttpMethod   = 'GET' 
-        )
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=message))
+# @handler.add(PostbackEvent)
+# def handle_postback(event):
+#     '''
+#     PostBackアクションがあったときの動作
+#     '''
+#     postbackdata = event.postback.data
+#     if postbackdata == "action:yes":
+#         # 着順を入力
+#         bucket.download_file(fname, fname)
+#         date = datetime.date.today()
+#         with open(fname,'a') as f:
+#             f.write("{},{},{},{},{}\n".format(date,data[0],data[1],data[2],data[3]))
+#         # bucket.upload_file(fname, fname)
+#         # 収支を入力
+#         with open(fname) as f:
+#             lines = f.readlines()
+#             tmp = lines[-1].rstrip('\n') + ",{}\n".format(money)
+#             lines[-1] = tmp
+#         with open(fname,'w') as f:
+#             for line in lines:
+#                 f.write(line)
+#         bucket.upload_file(fname, fname)
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             TextSendMessage(text="done!")
+#         )
+
+#     elif postbackdata == "request_pie":
+#         # 円グラフを表示
+#         bucket.download_file(fname, fname)
+#         df = pd.read_csv(fname,header=0)
+#         plt.clf()
+#         x = np.array([df["1st"].sum(),df["2nd"].sum(),df["3rd"].sum(),df["4th"].sum()])
+#         plt.pie(x)
+#         plt.legend()
+#         plt.savefig("pie.png")
+#         bucket.upload_file("pie.png", "pie.png")
+#         s3_image_url = s3_client.generate_presigned_url(
+#             ClientMethod = 'get_object',
+#             Params       = {'Bucket': aws_s3_bucket, 'Key': "pie.png"},
+#             ExpiresIn    = 10,
+#             HttpMethod   = 'GET'
+#         )
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             ImageSendMessage(
+#                 original_content_url = s3_image_url,
+#                 preview_image_url    = s3_image_url,
+#             )
+#         )
+#     elif postbackdata == "request_sum":
+#         # 合計を表示
+#         bucket.download_file(fname, fname)
+#         df = pd.read_csv(fname,header=0)
+#         x = np.array([df["1st"].sum(),df["2nd"].sum(),df["3rd"].sum(),df["4th"].sum()])
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             TextSendMessage(text = "{}-{}-{}-{}".format(x[0],x[1],x[2],x[3]))
+#         )
+#     elif postbackdata == "request_data":
+#         # データURLを表示
+#         message = s3_client.generate_presigned_url(
+#             ClientMethod = 'get_object',
+#             Params       = {'Bucket': aws_s3_bucket, 'Key': fname},
+#             ExpiresIn    = 10,
+#             HttpMethod   = 'GET' 
+#         )
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             TextSendMessage(text=message))
 
 
 
